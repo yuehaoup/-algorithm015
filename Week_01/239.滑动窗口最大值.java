ce6078a99ea3ca6cdd9daa293bf58dcf0feb9243
yuehaoup <yuehaoup@163.com>
2020-08-29 23:00:18 +0800

    LeetCode-239

---
+++ Week_01/239.滑动窗口最大值.java
@@ -0,0 +1,67 @@
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/*
+ * @lc app=leetcode.cn id=239 lang=java
+ *
+ * [239] 滑动窗口最大值
+ */
+
+// @lc code=start
+class Solution2 {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+
+        // Method 1: Violent
+        // TC O(nk) SC(n)
+        // Usage: exceed
+        // int[] ret = new int[nums.length - k + 1];
+        // Queue<Integer> queue = new LinkedList<>();
+        // Integer[] tmp = new Integer[k];
+        // for (int i = 0; i < nums.length; i++) {
+        //     queue.add(nums[i]);
+        //     if (queue.size() < k) {
+        //         continue;
+        //     }
+        //     if (queue.size() > k) {
+        //         queue.poll();
+        //     }
+        //     queue.toArray(tmp);
+        //     int max = tmp[0];
+        //     for (int j = 1; j < k; j++) {
+        //         max = Math.max(max, tmp[j]);
+        //     }
+        //     ret[i - k + 1] = max;
+        // }
+        // return ret;
+
+        // Method 2: DQ
+        // TC O(n) SC O(n)
+        // 18 ms	48.9 MB
+        int[] ret = new int[nums.length - k + 1];
+        ArrayDeque<Integer> queue = new ArrayDeque<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (queue.isEmpty() || nums[queue.peekFirst()] < nums[i]) {
+                queue.clear();
+            }
+            while (nums[queue.peekLast()] < nums[i]) {
+                queue.pollLast();
+            }
+            queue.addLast(i);
+            if (queue.peekFirst() <= i - k) {
+                queue.pollFirst();
+            }
+            if (i - k + 1 >= 0) {
+                ret[i - k + 1] = nums[queue.peekFirst()];
+            }
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        Solution2 ins = new Solution2();
+        ins.maxSlidingWindow(new int[]{1, -1}, 1);
+    }
+}
+// @lc code=end
+
